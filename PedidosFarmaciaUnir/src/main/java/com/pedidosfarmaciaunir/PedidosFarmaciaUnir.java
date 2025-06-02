354c72da68b885eca2141cb4217096065b8795ea
marco.celis <marco.celis@gmail.com>
2025-06-01 19:06:08 -0500

    primer commit

---
+++ PedidosFarmaciaUnir/src/main/java/com/pedidosfarmaciaunir/PedidosFarmaciaUnir.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pedidosfarmaciaunir;
+
+/**
+ *
+ * @author marco
+ */
+public class PedidosFarmaciaUnir {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
